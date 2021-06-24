7a2c607a5b371381d9ac6e8eecd73256e2bbc288
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-06-24 07:19:56 +0700

    initial commit

---
+++ src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    // public static void main(String[] args) throws Exception {
+    // System.out.println("Hello, World!");
+    // System.out.println("X");
+    // }
+
+    public static void main(String[] args) {
+        Xm xm = new Xm();
+    }
+
+}
+
+class Xm {
+    int a;
+    int b;
+    int c;
+
+    Xm() {
+        int a = 20;
+        int b = 30;
+        int c = 40;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
